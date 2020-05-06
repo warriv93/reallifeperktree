fc7f3e87ce65332f79f698f6fba55f6eade6af43
warriv93 <warriv93@gmail.com>
2020-05-06 17:07:24 +0200

    added login check för att lägga till perk data

--- src/pages/perktree/comp/PerkLevels.tsx
+++ src/pages/perktree/comp/PerkLevels.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Link from "next/link";
+import { getUserLoggedin } from "../../../api/userlocalstorage";
 
 import "../styles/perkLevel.scss";
 
@@ -11,15 +12,40 @@ type Props = {
   currentLevel: number;
 };
 
-const PerkLevel: React.FunctionComponent<Props> = ({grayscale, image, title, usedInPerkHeader, currentLevel}) => (
-  <li className="perk-level tooltip">
-    {/* If perklevels are in the header or the user has not pregressed the perk -> perkinput, otherwise perk learn more */}
-    <Link href={usedInPerkHeader || currentLevel <= 0 ? `/perkinput/${title}` : `/perk/${title}`}>
-      <img src={image} alt="perk-image" className={`perk-image ${grayscale}`}  />
-    </Link>
-    {/* if the perk level is not yet aquired show a tooltip on hover */}
-    {grayscale && <span className="perktooltiptext">Progress perk</span>}
-  </li>
-);
+const PerkLevel: React.FunctionComponent<Props> = ({
+  grayscale,
+  image,
+  title,
+  usedInPerkHeader,
+  currentLevel,
+}) => {
+  let link: string;
+
+  if (
+    (currentLevel == 0 && getUserLoggedin()) ||
+    (usedInPerkHeader && getUserLoggedin())
+  ) {
+    link = `/perkinput/${title}`;
+  } else if (!usedInPerkHeader) {
+    link = `/perk/${title}`;
+  } else {
+    link = "/login";
+  }
+
+  return (
+    <li className="perk-level tooltip">
+      {/* If perklevels are in the header or the user has not pregressed the perk -> perkinput, otherwise perk learn more */}
+      <Link href={link}>
+        <img
+          src={image}
+          alt="perk-image"
+          className={`perk-image ${grayscale}`}
+        />
+      </Link>
+      {/* if the perk level is not yet aquired show a tooltip on hover */}
+      {grayscale && <span className="perktooltiptext">Progress perk</span>}
+    </li>
+  );
+};
 
 export default PerkLevel;
