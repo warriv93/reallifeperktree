90236bd6b0b93caa5415b15f96fd8e961af271a7
warriv93 <warriv93@gmail.com>
2020-05-11 14:43:21 +0200

    iq done + small updates

--- src/pages/perkinput/comp/summary.tsx
+++ src/pages/perkinput/comp/summary.tsx
@@ -30,18 +30,21 @@ export default function summary({ answers, urlperk }: Props) {
   }, [urlperk]);
 
   function calculateScore(perk: IPerk) {
-    let newScore = 0;
-    let inputQuestiontype: boolean;
-    let perkSection: string;
+    let newScore = 0,
+      input = 0,
+      inputQuestiontype: boolean,
+      perkSection: string;
 
     answers.map((answer, index) => {
       inputQuestiontype = perk.questions[answer.question].type == 1 && true;
       perkSection = perk.questions[answer.question].section
         ? perk.questions[answer.question].section
         : "";
+      input = Number(answer.answer);
 
       console.log(answer);
 
+      // TODO: SCORE from LEVEL 1-5
       if (inputQuestiontype) {
         switch (perkSection) {
           case "BMI":
@@ -55,14 +58,12 @@ export default function summary({ answers, urlperk }: Props) {
 
           case "IQ":
             //SOURCE - IQ RANGE BELL CURVE https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fiqtestprep.com%2Fwp-content%2Fuploads%2F2018%2F06%2Fiq-distribution-curve-1.png&f=1&nofb=1
-            console.log(perkSection, " Answer: ", answer.answer);
-
-            newScore = answer.answer <= 70 && newScore;
-            newScore = answer.answer >= 85 && newScore + 1;
-            newScore = answer.answer >= 100 && newScore + 2;
-            newScore = answer.answer >= 115 && newScore + 3;
-            newScore = answer.answer >= 130 && newScore + 4;
-            newScore = newScore + answer.answer;
+            // console.log(perkSection, " Answer: ", input);
+            newScore += input >= 70 ? 1 : 0;
+            newScore += input >= 85 ? 1 : 0;
+            newScore += input >= 100 ? 1 : 0;
+            newScore += input >= 115 ? 1 : 0;
+            newScore += input >= 130 ? 1 : 0;
             break;
 
           case "Stamina":
