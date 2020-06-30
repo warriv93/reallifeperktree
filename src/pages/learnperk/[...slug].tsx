cb1949ed2fff422c0cefb645c68247bee4613d74
warriv93 <warriv93@gmail.com>
2020-06-30 15:08:22 +0200

    rename

---
+++ src/pages/learnperk/[...slug].tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import Layout from "../../components/layout";
+
+//catches wrong urls
+//Dynamic routes can be extended to catch all paths by adding three dots (...) inside the brackets. For example:
+//pages/post/[...slug].js matches /post/a, but also /post/a/b, /post/a/b/c and so on.
+// https://nextjs.org/docs/routing/dynamic-routes
+const Slug = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  return (
+    <Layout>
+      <p>Page {slug} does not exist</p>
+    </Layout>
+  );
+};
+
+export default Slug;
