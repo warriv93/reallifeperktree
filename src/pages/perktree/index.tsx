d0898987e35128c5d475e50794e96cce9ea3e530
warriv93 <warriv93@gmail.com>
2020-06-30 15:47:42 +0200

    comment to perktree architecture

--- src/pages/perktree/index.tsx
+++ src/pages/perktree/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../../components/layout";
 import Perktree from "./comp/perktree";
 
+// allows home index page to render only the perktree component without the Layout (header etc)
 export default function perktree() {
   return (
     <Layout>
