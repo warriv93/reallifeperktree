77680c8d435bbfb22ab5d0a32748c101bcde6f02
warriv93 <warriv93@gmail.com>
2020-06-30 15:18:38 +0200

    Revert "refactor, just moved code to container and eliminated a component"
    This reverts commit 26263aa795d07704efeeeb2c2b8acd9e402abaad.

--- src/pages/perktree/index.tsx
+++ src/pages/perktree/index.tsx
@@ -1,32 +1,10 @@
 import Layout from "../../components/layout";
-import { useEffect, useState } from "react";
-import PerkHorizontalList from "./comp/perkhorizontallist";
-
-import { getUserPerks } from "../../api/userlocalstorage";
-import { perkList } from "../../api/";
-import "./styles/index.scss";
+import Perktree from "./comp/perktree";
 
 export default function perktree() {
-  const [perks, setPerks] = useState(perkList);
-
-  useEffect(() => {
-    getUserPerks().then((perks) => perks && setPerks(perks));
-    // If we pass an empty array to useEffect, it’s only executed after the first render. https://www.andreasreiterer.at/react-useeffect-hook-loop/
-  }, []);
-
   return (
     <Layout>
-      <div className="perktree-container">
-        {perks &&
-          perks.map((perk) => (
-            <PerkHorizontalList
-              key={perk.id}
-              level={perk.level}
-              title={perk.title}
-              image={perk.image}
-            />
-          ))}
-      </div>
+      <Perktree />
     </Layout>
   );
 }
